1dbb743eb65589158099ebb79acb04b598d3b0aa
descolar-ceo <emmanuellamugi@gmail.com>
2020-04-30 10:13:40 +0200

    chore: Increase Maintainability
    -Increase code maintainability by refactoring the codebase to change codeclimate badge
    
    [Finishes #172599039]

--- App.js
+++ App.js
@@ -1,30 +1,12 @@
 import { createSwitchNavigator, createAppContainer } from 'react-navigation';
-import CallLogPermsReqsScreen from './src/screens/permissionScreen/CallLogPermsReqsScreen';
-import WelcomeScreen from './src/screens/WelcomeScreen';
-import DashbordScreen from './src/screens/DashboardScreen';
-import SmsPermsReqsScreen from './src/screens/permissionScreen/SmsPermsReqsScreen';
-import SmsListScreen from './src/screens/listingScreens/sms/SmsListScreen';
-import IncomingCallScreen from './src/screens/listingScreens/calls/IncomingCallScreen';
-import OutgoingCallScreen from './src/screens/listingScreens/calls/OutgoingCallScreen';
-import MissedCallScreen from './src/screens/listingScreens/calls/MissedCallScreen';
-import RejectedCallScreen from './src/screens/listingScreens/calls/RejectedCallScreen';
-import BlockedCallScreen from './src/screens/listingScreens/calls/BlockedCallScreen';
+import SignupScreen from './src/screens/signup/SignupScreen';
 
 const navigator = createSwitchNavigator(
   {
-    WelcomeScreen,
-    DashbordScreen,
-    CallLogPermsReqsScreen,
-    SmsPermsReqsScreen,
-    SmsListScreen,
-    IncomingCallScreen,
-    OutgoingCallScreen,
-    MissedCallScreen,
-    RejectedCallScreen,
-    BlockedCallScreen
+    SignupScreen,
   },
   {
-    initialRouteName: 'DashbordScreen',
+    initialRouteName: 'SignupScreen',
     defaultNavigationOptions: {
       title: 'Phone History Store',
     },
--- src/styles/allStyles.js
+++ src/styles/allStyles.js
@@ -1,83 +1,55 @@
 import { StyleSheet } from 'react-native';
 
 const allStyles = StyleSheet.create({
-  container: {
-    backgroundColor: 'rgb(100,120,255)',
-    flex: 1,
+  signupPageIconContainer: {
+    height: '0.5%',
+  },
+  signupFormContainer: {
+    backgroundColor: 'rgba(90,100,100,0.8)',
+    width: '100%',
+    height: '100%',
+    padding: '2%',
+    display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
-    alignItems: 'center',
-    padding: 5,
   },
-  text: {
-    fontSize: 15,
-    color: 'rgb(255,255,255)',
-    textAlign: 'center',
-  },
-  touchableBtn: {
-    marginTop: -20,
+  signupTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginLeft: 'auto',
+    marginRight: 'auto',
     marginBottom: 10,
-    padding: 5,
-    width: '80%',
-    borderRadius: 30,
-    backgroundColor: 'rgba(200,200,220,0.8)',
+    color: 'rgb(255,255,255)',
   },
-  dashBoardContainer: {
-    backgroundColor: '#4B4A67',
-    height: '100%',
-    padding: '10%',
-    justifyContent: 'center',
+  signupInput: {
+    color: 'rgb(255,255,255)',
+    backgroundColor: 'rgba(255,255,255,0.1)',
+    borderRadius: 10,
+    fontSize: 18,
+    width: '100%',
+    marginBottom: 5,
   },
-  dashBoardBtns: {
+  signupBtn: {
     padding: '4%',
     margin: '1%',
-    width: '100%',
+    width: '50%',
     borderRadius: 50,
     backgroundColor: 'rgb(200,200,220)',
   },
-  dashboardText: {
-    textAlign: 'center',
-    fontSize: 18,
-  },
-  iconContainer: {
-    marginTop: 50,
-    height: 350,
-    width: 350,
-  },
-  welcomeContent: {
-    backgroundColor: 'rgba(255,255,255,0.3)',
-    marginTop: 10,
-    padding: 10,
-    borderRadius: 15,
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignContent: 'center',
-  },
-  welcomeText: {
-    color: '#FFFFFF',
-    fontSize: 15,
-    textAlign: 'center',
-    marginBottom: 20,
-    marginTop: 20,
-  },
-  listingMainContainer: {
-    justifyContent: 'center',
-    flex: 1,
-    marginLeft: 10,
-    marginRight: 10,
-    marginBottom: 10,
-    marginTop: 30,
-  },
-  listingItem: {
-    fontSize: 18,
-    padding: 10,
-  },
-  flatlistItemSeparatorStyles: {
-    height: 1,
+  nextBtn: {
+    height: 60,
     width: '100%',
-    backgroundColor: 'rgb(0,0,0)',
+    backgroundColor: 'rgba(255,255,255,0.5)',
+    borderRadius: 10,
+    padding: 10,
+    marginTop: 10,
+
+  },
+  nextText: {
+    color: 'rgb(255,255,255)',
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
-
-export default allStyles;
+export default { allStyles };
