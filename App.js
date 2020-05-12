cd239b49ae659bfe112dd5b001c7616ebbf3b7b3
descolar-ceo <emmanuellamugi@gmail.com>
2020-05-12 14:53:56 +0200

    bug: Add provider
    [Finishes #172793800]

--- App.js
+++ App.js
@@ -2,8 +2,10 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Provider } from 'react-redux';
 import SignupScreen from './src/screens/signup/SignupScreen';
 import WelcomeScreen from './src/screens/welcome/WelcomeScreen';
+import store from './src/store';
 
 /**
  * @return {*} renders the UI
@@ -11,20 +13,22 @@ import WelcomeScreen from './src/screens/welcome/WelcomeScreen';
 const App = () => {
   const Stack = createStackNavigator();
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          options={{ headerShown: false }}
-          name="WelcomeScreen"
-          component={WelcomeScreen}
-        />
-        <Stack.Screen
-          name="SignupScreen"
-          component={SignupScreen}
-          options={{ title: 'Signup' }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="WelcomeScreen"
+            component={WelcomeScreen}
+          />
+          <Stack.Screen
+            name="SignupScreen"
+            component={SignupScreen}
+            options={{ title: 'Signup' }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 };
 
