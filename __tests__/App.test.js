328444846eb27801c90d512c3ad5c989d04acbb7
descholar-ceo <emmanuellamugi@gmail.com>
2020-05-19 00:51:45 +0200

    chore: Setup unit test
    -Try to fix the issues
    
    [Finishes #]

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,25 +1,25 @@
-// /**
-//  * @format
-//  */
+/**
+ * @format
+ */
 
-// import 'react-native';
-// import React from 'react';
-// import App from '../App';
-
-// // Note: test renderer must be required after react-native.
-// import renderer from 'react-test-renderer';
-
-// it('renders correctly', () => {
-//   renderer.create(<App />);
-// });
+import 'react-native';
+import React from 'react';
 import App from '../App';
-import SignupScreen from '../../src/screens/signup/SignupScreen';
-import {shallow}from 'enzyme';
 
-describe('Signup test', ()=>{
-    it('Will show signup screen', ()=>{
-        const wrapper = shallow(<App/>);
-        expect(wrapper.contains(<SignupScreen/>))
-    });
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+
+it('Renders correctly', () => {
+  renderer.create(<App />);
 });
+// import App from '../App';
+// import SignupScreen from '../../src/screens/signup/SignupScreen';
+// import {shallow}from 'enzyme';
+
+// describe('Signup test', ()=>{
+//     it('Will show signup screen', ()=>{
+//         const wrapper = shallow(<App/>);
+//         expect(wrapper.contains(<SignupScreen/>))
+//     });
+// });
 
