169eef4755259dc725aa8b2bd155a19dff0b3feb
descholar-ceo <emmanuellamugi@gmail.com>
2020-05-19 15:06:54 +0200

    Setup profile
    -Customize the tabs
    
    [Finishes #172881194]

--- src/screens/profile/userProfileScreen.js
+++ src/screens/profile/userProfileScreen.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import jwtDecode from 'jwt-decode';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import getUserData from '../../actions/getUserData';
 import ProfileTab from './Tabs/profileTab';
 import StoreTab from './Tabs/storeTab';
@@ -34,16 +35,36 @@ class UserProfileScreen extends Component {
      */
   render() {
     return (
-      <Tab.Navigator>
-        <Tab.Screen
-          name="Profile"
-          component={ProfileTab}
-        />
-        <Tab.Screen
-          name="Store"
-          component={StoreTab}
-        />
-      </Tab.Navigator>
+      <>
+        <Tab.Navigator
+          tabBarOptions={{
+            activeTintColor: 'tomato',
+            inactiveTintColor: 'gray',
+          }}
+        >
+          <Tab.Screen
+            name="Profile"
+            component={ProfileTab}
+            options={{
+              tabBarLabel: 'My Profile',
+              tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons name="account" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Store"
+            component={StoreTab}
+            options={{
+              tabBarLabel: 'My Store',
+              tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons name="store" color={color} size={size} />
+              ),
+
+            }}
+          />
+        </Tab.Navigator>
+      </>
     );
   }
 }
