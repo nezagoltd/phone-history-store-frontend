593c9a9a21a86099dbae4bbe28e6bef97e43050f
descholar <emmanuellamugi@gmail.com>
2020-05-24 21:51:47 +0200

    chore: Setup unit test
    -Remove jest teting
    
    [Finishes #172753236]

--- __tests__/screens/SignupScreen.test.js
+++
@@ -1,8 +0,0 @@
-import PhoneNumberScreen from '../../src/screens/signup/PhoneNumberScreen';
-import {shallow}from 'enzyme';
-
-describe('Signup test', ()=>{
-    it('Will show signup screen', ()=>{
-        const wrapper = shallow(<PhoneNumberScreen/>);
-    });
-});
